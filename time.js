fa7af6b9f733cbee51b368ef2f062acb77987331
sidaliscc3 <sidaliscala350@gmail.com>
2026-07-28 00:51:56 +0100

    some api

--- time.js
+++ time.js
@@ -1,5 +1,11 @@
-fetch('https://api.aladhan.com/v1/timingsByCity/01-01-2025?city=London&country=GB&state=London&x7xapikey=&method=3&shafaq=general&tune=5%2C3%2C5%2C7%2C9%2C-1%2C0%2C8%2C-6&school=0&midnightMode=0&timezonestring=UTC&latitudeAdjustmentMethod=1&calendarMethod=UAQ&iso8601=false', {
-  headers: {
-    'Accept-Encoding': ''
-  }
-})
+fetch('https://api.aladhan.com/v1/timingsByCity/20-05-2025?city=algeria&country=GB')
+.then(response => response.json())
+  .then(data => {
+    console.log(data.data.timings);
+  });
+ 
+fetch('https://countriesnow.space/api/v0.1/countries')
+  .then(response=>response.json())
+  .then(data=> {
+    console.log(data)
+  });
